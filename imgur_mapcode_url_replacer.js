737e39abdd8a537ed0d972f873e2ff80d1254f4b
GkunTFM <gekkeiju.tfm@gmail.com>
2016-12-13 16:27:41 +0900

    imgur url replacer was added

---
+++ imgur_mapcode_url_replacer.js
@@ -0,0 +1,24 @@
+//http://imgur.com/a/PI3vT
+//excute this script on the album page
+(function showUI(){
+	var ta = document.createElement("textarea");
+	ta.style.height = 200;
+	ta.style.width = 300;
+	document.body.insertBefore(ta, document.body.firstChild);
+	var button = document.createElement("input");
+	button.type = "button";
+	button.value = "decode printls string";
+	button.onclick = function(){
+		var imgs = _widgetFactory._.config.gallery.image.album_images.images;
+		var data = [];
+		var str = ta.value;
+		var img;
+		for(var i = 0; i < imgs.length; i++){
+			img = imgs[i];
+			str = str.replace("!" + img.name, "http://i.imgur.com/" + img.hash + img.ext);
+		}
+		ta.value = str;
+		console.log("done");
+	};
+	document.body.insertBefore(button, document.body.firstChild);
+})();
